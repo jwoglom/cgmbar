a30e402b463d404769aca8aecd38d9102ea61b4b
James Woglom <j@wogloms.net>
2021-05-17 22:47:54 -0400

    add right-click menu, single-click to update

--- CGMBar/AppDelegate.swift
+++ CGMBar/AppDelegate.swift
@@ -9,49 +9,58 @@ import Cocoa
 import SwiftUI
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
-    var popover: NSPopover!
     var statusBarItem: NSStatusItem!
+    var statusBarMenu: NSMenu!
     var updateTimer: Timer!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-        
-        // Create the popover
-        let popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 400)
-        popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: contentView)
-        self.popover = popover
-        
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+
+        self.statusBarMenu = NSMenu(title: "CGMBar")
+
+        self.statusBarMenu = NSMenu()
+        if let menu = self.statusBarMenu {
+            menu.addItem(withTitle: "Update Now", action: #selector(updateBar), keyEquivalent: "u")
+            menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+            menu.delegate = self
+        }
+
 
         if let button = self.statusBarItem.button {
              //button.image = NSImage(named: "Icon")
-             button.title = "..."
-             button.action = #selector(togglePopover(_:))
+            button.action = #selector(onClick)
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
-        
+
+        //self.statusBarItem.button?.menu = statusBarMenu
+
         self.updateTimer = Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(updateBar), userInfo: nil, repeats: true)
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300)) {
             self.updateBar()
         }
 
     }
     
-    // Create the status item
-    @objc func togglePopover(_ sender: AnyObject?) {
-         if let button = self.statusBarItem.button {
-              if self.popover.isShown {
-                   self.popover.performClose(sender)
-              } else {
-                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-              }
-         }
+    @objc func onClick(sender: NSStatusItem) {
+        let event = NSApp.currentEvent!
+        
+        if event.type == NSEvent.EventType.rightMouseUp {
+            statusBarItem.menu = statusBarMenu
+            statusBarMenu.popUp(positioning: nil,
+                                at: NSPoint(x: 0, y: statusBarItem.statusBar!.thickness),
+                                in: statusBarItem.button)
+            
+        } else {
+            updateBar()
+        }
+    }
+    
+    @objc func menuDidClose(_ menu: NSMenu) {
+        statusBarItem.menu = nil
     }
     
     struct SGVReading: Decodable {
@@ -67,6 +76,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // Obtains the latest SGV reading and updates the statusbar
     @objc func updateBar() {
         print("Running updateBar")
+        if let button = self.statusBarItem.button {
+            DispatchQueue.main.async {
+                button.title = "..."
+            }
+        }
         let url = URL(string: "http://127.0.0.1:1337/api/v1/entries/sgv.json?count=1")!
         
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
